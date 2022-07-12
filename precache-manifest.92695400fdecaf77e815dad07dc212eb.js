f92c83ba902286d57013f853a5b4e99ff8d2344b
Rodion <50911976+RodionPushkin@users.noreply.github.com>
2022-07-12 20:17:22 +0300

    Add files via upload

---
+++ precache-manifest.92695400fdecaf77e815dad07dc212eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2fe09d6c8d76d94a415",
+    "url": "/css/app.5cb96396.css"
+  },
+  {
+    "revision": "2a5343841af50d6a20e19d35d7f27c75",
+    "url": "/img/9f664359381ff7e3d34f99bb86d7a3e0.jpg"
+  },
+  {
+    "revision": "c556b12aa6e11eb03a9124061cd58775",
+    "url": "/img/okean-zakat-volna-voda-priroda.jpg"
+  },
+  {
+    "revision": "8b176aabc8f28be89e7e526f256b82c1",
+    "url": "/img/ookean2.jpg"
+  },
+  {
+    "revision": "e114e3a1ad7493cc52edf1496119ae7c",
+    "url": "/img/pexels-photo-57946.jpg"
+  },
+  {
+    "revision": "0a1123dedb771c0fa23ce93a5af55221",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2fe09d6c8d76d94a415",
+    "url": "/js/app.15d75b6a.js"
+  },
+  {
+    "revision": "01e1e9fc3864e1c67d7e",
+    "url": "/js/chunk-vendors.3a47590d.js"
+  },
+  {
+    "revision": "31cde321b9a73bcb2cf0196924a03f36",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
